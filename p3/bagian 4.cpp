94115e66143a27f7dd020e5925aa77a9b85d2b55
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-02 12:50:19 +0700

    Add files via upload

---
+++ p3/bagian 4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "masukan panjang :"; >>
+    cin >> panjang;
+    cout <<"masukan lebar;";
+    cin >> lebar;
+}
