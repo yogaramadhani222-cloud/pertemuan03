020a44640a50c1f65d2d96bc79fabdd95bb5d78b
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-02 13:02:35 +0700

    Add files via upload

---
+++ p3/bagian 6.cpp
@@ -0,0 +1,12 @@
+void output()
+{
+    cout <<"luasnya :"<< luaspersegi
+    (panjang, lebar)<< endl;
+}
+int main()
+{
+    input();
+    output();
+    cout <<"kelilingnya : "<<kelilingPersegi
+    (panjang, lebar);
+}
